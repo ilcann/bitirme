0027efe6b3cec6fb32576f68700fa8cf7e6ba9ab
ilcann <ilcan21@itu.edu.tr>
2026-01-17 20:28:17 +0300

    refactor: update language and theme toggles to use menubar

--- src/components/common/language-toggle.tsx
+++ src/components/common/language-toggle.tsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import { lngs, type LangKey } from '@/config/languages';
 import { useLanguage } from '@/providers/language-provider';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu';
 import { Button } from '../ui/button';
 import { Globe } from 'lucide-react';
+import {
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
 export const LanguageToggle: React.FC = () => {
   const { lang, setLang } = useLanguage();
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <MenubarMenu>
+      <MenubarTrigger asChild>
         <Button variant="ghost" size="icon">
           <Globe className="h-5 w-5" />
         </Button>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuContent align="end">
+      </MenubarTrigger>
+      <MenubarContent align="end">
         {Object.entries(lngs).map(([code, { nativeName }]) => (
-          <DropdownMenuItem
+          <MenubarItem
             key={code}
             onClick={() => setLang(code as LangKey)}
             disabled={code === lang}
-            className="px-2 py-1 rounded border"
           >
             {nativeName}
-          </DropdownMenuItem>
+          </MenubarItem>
         ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </MenubarContent>
+    </MenubarMenu>
   )
 };
--- src/components/common/theme-toggle.tsx
+++ src/components/common/theme-toggle.tsx
@@ -2,36 +2,38 @@ import { Moon, Sun } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 import { useTheme } from "@/providers/theme-provider"
+import { useTranslation } from "react-i18next";
 
 export function ThemeToggle() {
   const { setTheme } = useTheme()
+  const { t } = useTranslation();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+    <MenubarMenu>
+      <MenubarTrigger asChild>
+        <Button variant="ghost" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">{t("theme_toggle.title")}</span>
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </MenubarTrigger>
+      <MenubarContent align="end">
+        <MenubarItem onClick={() => setTheme("light")}>
+          {t("theme_toggle.light")}
+        </MenubarItem>
+        <MenubarItem onClick={() => setTheme("dark")}>
+          {t("theme_toggle.dark")}
+        </MenubarItem>
+        <MenubarItem onClick={() => setTheme("system")}>
+          {t("theme_toggle.system")}
+        </MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
   )
 }
