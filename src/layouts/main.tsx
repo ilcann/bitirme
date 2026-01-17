6f0c45467802514d3994d250a03238d595cf8883
ilcann <ilcan21@itu.edu.tr>
2026-01-17 20:29:01 +0300

    feat: add app header to main layout

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,7 +1,11 @@
+import AppHeader from "@/components/common/app-header"
 import { Outlet } from "react-router"
 
 export default function MainLayout() {
   return (
-    <Outlet />
+    <>
+        <AppHeader />
+        <Outlet />
+    </>
   )
 }
